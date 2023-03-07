e4f04c51d3db382ba31df7acc28bb555f7873c94
Nayjk <98636064+nayjk@users.noreply.github.com>
2023-03-07 11:03:32 +0300

    Add files via upload

---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,67 @@
+﻿
+class Animal
+{
+    string food;
+    string location;
+    string name;
+    Animal animal;
+    public void makeNoise()
+    {
+        
+    }
+
+    public void eat()
+    {
+
+    }
+
+    public void sleep()
+    {
+
+    }
+
+    public Animal(string food, string location, string name,Animal animal)
+    {
+        this.food = food;
+        this.location = location;
+        this.name = name;
+        this.animal = animal;
+    }
+}
+
+class Dog : Animal
+{
+    public Dog(string food,string location, string name) : base(food,location,name)
+    {
+        
+    }
+}
+
+class Cat : Animal
+{
+    public Cat(string food, string location, string name) : base(food,location,name)
+    {
+
+    }
+}
+
+class Horse : Animal
+{
+    public Horse(string food,string location,string name) : base(food,location,name)
+    {
+
+    }
+}
+
+class Veterinar
+{
+    public Veterinar(string food, string location,string name) : base(food,location,name)
+    {
+
+    }
+
+    public void treatAnimal(Animal animal)
+    {
+        Console.WriteLine()
+    }
+}
